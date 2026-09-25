10610563e69511b0b500f159a7692924b67efa04
SPIN0ZAi <soulaimane.benlouaret1@alu.uclm.es>
2026-09-25 09:47:13 +0200

    Adding validatiom rules

--- src/AppForSEII.API/Models/Team.cs
+++ src/AppForSEII.API/Models/Team.cs
@@ -10,6 +10,9 @@ public class Team
     [Key]
     public int Id { get; set; }
 
-
-    public string Name { get; set; } = "Team name";
+    [StringLength(10,MinimumLength = 3)]
+    public string Name { get; set; } = "Team Name";
+    public string Description { get; set; } = "Team Name";
+    // new attribute of application user and gender
+    
 }
